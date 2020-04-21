17e5c102bd9c80745b2d7dcb683e53c1fcadff6f
Steven Ferreira <57821462+steven-f206@users.noreply.github.com>
2020-04-21 12:27:12 -0400

    Add employee functionality created

--- app.js
+++ app.js
@@ -4,42 +4,8 @@ const cTable = require('console.table');
 const cli = require('./lib/cli');
 
 // Create Connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Phoenix1',
-    database: 'employeetracker_db',
-    port: 3306
-});
-
-//Connect DB
-
 let init = () => {
-    connection.connect();
-    console.log("----------------------------------------------------");
-    console.log("                 Employee Tracker                   ");
-    console.log("----------------------------------------------------");
-
-    connection.query(`
-        SELECT * FROM employee 
-        INNER JOIN role on role.id = employee.id
-        INNER JOIN department on department.id = role.department_id;`,
-        (err, res) => {
-            let employeeTable = [];
-            res.forEach((employee) => {
-                employeeTable.push({ 'id': employee.id, 'First Name': employee.first_name, 'Last Name': employee.last_name, 'Title': employee.role_id, 'Manager': employee.manager_id });
-            });
-            // console.log(employeeTable);
-
-            console.table(
-                employeeTable
-            );
-            cli();
-        });
-
-    connection.end();
-
+    cli();
 };
-
 
 init(); 
--- assets/mysql/createEmployee.sql
+++ assets/mysql/createEmployee.sql
@@ -53,11 +53,40 @@ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
 ('Nim', 'Nam', 6, NULL),
 ('Vim', 'Vam', 7, 6);
 
-SELECT * FROM employee;
+SELECT * FROM role;
 
 SELECT * FROM employee
-INNER JOIN role on role.id = employee.id
-INNER JOIN department on department.id = role.department_id;
+LEFT JOIN role on employee.role_id = role.id
+LEFT JOIN department on role.department_id = department.id;
+
+
+/*Select manager employees*/
+SELECT e1.manager_id, CONCAT(e2.first_name, ' ', e2.last_name) AS manager FROM employee as e1
+INNER JOIN employee as e2 on e2.id = e1.manager_id;
+
+/*Select all employees*/
+SELECT e1.id, e1.first_name, e1.last_name, role.title, department.name AS department, role.salary, CONCAT(e2.first_name, ' ', e2.last_name) AS manager FROM employee as e1
+LEFT JOIN role on e1.role_id = role.id
+LEFT JOIN department on role.department_id = department.id
+LEFT JOIN employee as e2 on e2.id = e1.manager_id;
+
+/*Select all employees by department*/
+SELECT e1.id, e1.first_name, e1.last_name, role.title, department.name AS department, role.salary, CONCAT(e2.first_name, ' ', e2.last_name) AS manager FROM employee as e1
+LEFT JOIN role on e1.role_id = role.id
+LEFT JOIN department on role.department_id = department.id
+LEFT JOIN employee as e2 on e2.id = e1.manager_id
+ORDER BY department ASC;
+
+/*Select all employees by manager*/
+SELECT CONCAT(e2.first_name, ' ', e2.last_name) AS manager, e1.id, e1.first_name, e1.last_name, role.title, department.name AS department, role.salary FROM employee as e1
+LEFT JOIN role on e1.role_id = role.id
+LEFT JOIN department on role.department_id = department.id
+INNER JOIN employee as e2 on e2.id = e1.manager_id
+ORDER BY manager ASC ;
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id ) VALUES ('TEST', 'DELETE', 1, 1);
+DELETE FROM employee WHERE first_name = 'TEST' ;
 
 /*INNER JOIN employee on employee.manager_id = employee.id*/
 
